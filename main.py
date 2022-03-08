998af08d4274f4ff470e1a7a1d446c4b5989fa11
Thiago Bischoff <76192049+Zigdeck@users.noreply.github.com>
2022-03-08 18:49:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from music import Music
+from musicProperties import Music
 
+texto = "ElaBPM-BPM-borar uma lista-+++ (informal, em porT+tuguês) dos req--++uisitos que o sisBPM+tema deveBPM+ possuir no pT-onto de vista dos autor"
 instrumento = "Violao"
 volume_inicial = 10
 bpm = 50
 oitava = 5
-termo = "+"
 
 m1 = Music(instrumento, volume_inicial, volume_inicial, bpm, oitava)
-
 print(f"instumento: {m1.instrumento}")
 print(f"volume: {m1.volume_inicial}")
 print(f"bpm: {m1.bpm}")
 print(f"oitava: {m1.oitava}")
+i = 0
 
-m1.volume_changed("-")
-m1.volume_changed("-")
-
-m1.bpm_changed("BPM-")
-m1.bpm_changed("BPM+")
-
-
-
+""" Teste para reproduzir o texto e ver o resultado no console """
+while i < len(texto):
+    if texto[i:i+3] == "BPM":
+        m1.tratamento_caractere(texto[i:i+4])
+        i += 4
+    elif texto[i:i+2] == "T+" or texto[i:i+2] == "T-":
+        m1.tratamento_caractere(texto[i:i+2])
+        i += 2
+    else:
+        m1.tratamento_caractere(texto[i])
+        i += 1
